b7338915a0fa5220c810a626738d8066b0ea1fb3
alexgab <alexgabriel3357@gmail.com>
2024-07-05 22:05:16 -0300

    criação da estrutura dos componentes

--- aluraGeek/src/app/app.module.ts
+++ aluraGeek/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+import { ListaProdutosComponent } from './componentes/lista-produtos/lista-produtos.component';
+import { CardComponent } from './componentes/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ListaProdutosComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
